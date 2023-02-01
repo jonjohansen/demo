77191042f31e0e61d17ce00d600c77cf05adc58c
Jon Johansen <jonjohansen2k@gmail.com>
2023-02-01 20:32:56 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
+import { Poppins } from '@next/font/google'
+
+export const poppins = Poppins({subsets: ["latin"], weight: ["400"]})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={poppins.className}><Component {...pageProps} /></main>
 }
